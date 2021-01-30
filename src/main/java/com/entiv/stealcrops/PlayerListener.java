4b9ef5fcb239e45d308e43e83ef7956c9e606175
EnTIv <1522935501@qq.com>
2021-01-30 16:36:11 +0800

    取消需要空手才能偷菜的设定

--- src/main/java/com/entiv/stealcrops/PlayerListener.java
+++ src/main/java/com/entiv/stealcrops/PlayerListener.java
@@ -7,6 +7,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -18,12 +19,10 @@ public class PlayerListener implements Listener {
     public void onPlayerInteract(PlayerInteractEvent event) {
 
         Block block = event.getClickedBlock();
+        if (!event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) return;
 
         Player player = event.getPlayer();
 
-        ItemStack item = player.getInventory().getItemInMainHand();
-
-        if (!item.getType().equals(Material.AIR)) return;
         if (block == null) return;
 
         boolean isEnableWorld = config.getStringList("启用的世界").contains(block.getWorld().getName());
